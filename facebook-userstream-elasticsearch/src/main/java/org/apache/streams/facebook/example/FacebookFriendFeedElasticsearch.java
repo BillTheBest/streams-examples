70d6455b7b4c19687d2c129262b966d0b23df3cc
sblackmon <sblackmon@w2odigital.com>
2014-07-07 09:45:22 -0500

    Facebook provider examples

---
+++ facebook-userstream-elasticsearch/src/main/java/org/apache/streams/facebook/example/FacebookFriendFeedElasticsearch.java
@@ -0,0 +1,65 @@
+package org.apache.streams.facebook.example;
+
+import com.facebook.processor.FacebookTypeConverter;
+import com.facebook.provider.FacebookFriendFeedProvider;
+import com.facebook.provider.FacebookFriendUpdatesProvider;
+import com.facebook.provider.FacebookUserstreamProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigRenderOptions;
+import org.apache.streams.config.StreamsConfigurator;
+import org.apache.streams.core.StreamBuilder;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.elasticsearch.ElasticsearchConfigurator;
+import org.apache.streams.elasticsearch.ElasticsearchPersistWriter;
+import org.apache.streams.elasticsearch.ElasticsearchWriterConfiguration;
+import org.apache.streams.facebook.FacebookUserstreamConfiguration;
+import org.apache.streams.jackson.StreamsJacksonMapper;
+import org.apache.streams.local.builders.LocalStreamBuilder;
+import org.apache.streams.pojo.json.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by sblackmon on 12/10/13.
+ */
+public class FacebookFriendFeedElasticsearch {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(FacebookFriendFeedElasticsearch.class);
+
+    private static final ObjectMapper MAPPER = new StreamsJacksonMapper();
+
+    public static void main(String[] args)
+    {
+        LOGGER.info(StreamsConfigurator.config.toString());
+
+        Config facebook = StreamsConfigurator.config.getConfig("facebook");
+        Config elasticsearch = StreamsConfigurator.config.getConfig("elasticsearch");
+
+        FacebookUserstreamConfiguration facebookUserstreamConfiguration;
+        try {
+            facebookUserstreamConfiguration = MAPPER.readValue(facebook.root().render(ConfigRenderOptions.concise()), FacebookUserstreamConfiguration.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        FacebookFriendFeedProvider provider = new FacebookFriendFeedProvider(facebookUserstreamConfiguration, ObjectNode.class);
+        FacebookTypeConverter converter = new FacebookTypeConverter(ObjectNode.class, Activity.class);
+        ElasticsearchWriterConfiguration elasticsearchWriterConfiguration = ElasticsearchConfigurator.detectWriterConfiguration(elasticsearch);
+        ElasticsearchPersistWriter writer = new ElasticsearchPersistWriter(elasticsearchWriterConfiguration);
+
+        StreamBuilder builder = new LocalStreamBuilder(new LinkedBlockingQueue<StreamsDatum>());
+
+        builder.newPerpetualStream(FacebookFriendFeedProvider.STREAMS_ID, provider);
+        builder.addStreamsProcessor("converter", converter, 2, FacebookFriendFeedProvider.STREAMS_ID);
+        builder.addStreamsPersistWriter("console", writer, 1, "converter");
+        builder.start();
+
+    }
+
+}
